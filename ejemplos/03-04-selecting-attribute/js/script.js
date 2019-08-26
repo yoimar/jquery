e78634f57c0f0c176c066a1ff747a95518678896
yoimar <urbinayoimar@gmail.com>
2019-08-25 21:34:02 -0400

    Clases de atravesando el dom
    Segunda Parte de las clases de atravesando el DOM

--- ejemplos/03-04-selecting-attribute/js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	//$('input[required]').addClass('highlighted');
-	//$('input[placeholder="Phone"]').addClass('highlighted');
-	$('input[placeholder*="Name"]').addClass('highlighted');
-});
