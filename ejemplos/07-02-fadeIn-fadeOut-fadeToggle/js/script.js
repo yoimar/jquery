c1ee88aa9239b64e1829602960199efa84032c9e
yoimar <urbinayoimar@gmail.com>
2019-09-04 23:12:18 -0400

    Animaciones y Efectos

--- ejemplos/07-02-fadeIn-fadeOut-fadeToggle/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	
-	$('.item-box').on('click', '.info-link', function(event) {
-		event.preventDefault();
-		//fadeIn(), fadeOut(), fadeToggle()
-		$(this).closest('.item-box').find('.more-info').fadeToggle(1000);
-	});
-});
